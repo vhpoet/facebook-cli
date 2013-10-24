6b83d83232ce45395c2a86f104442e8b1bfe4022
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-10-23 19:54:57 -0700

    Add colors.

--- facebook-cli.js
+++ facebook-cli.js
@@ -87,8 +87,8 @@ program
 
     fb.me(function(data){
       console.log();
-      console.log('You are ' + data.name + '. Your ID is ' + data.id);
-      console.log('Link to your profile: https://www.facebook.com/' + data.username);
+      console.log('You are '.grey + data.name.bold.cyan + '. Your ID is '.grey + data.id.bold.cyan);
+      console.log(('Link to your profile: ' + ('https://www.facebook.com/' + data.username).underline).grey);
       console.log();
     });
   });
@@ -102,8 +102,8 @@ program
     // TODO msg without " quotes
     fb.post(msg,function(data){
       console.log();
-      console.log('Status update has been posted.');
-      console.log('Here is the link: https://www.facebook.com/' + data.id);
+      console.log('Status update has been posted.'.green);
+      console.log(('Here is the link: ' + ('https://www.facebook.com/' + data.id).underline).grey);
       console.log();
     });
   });
